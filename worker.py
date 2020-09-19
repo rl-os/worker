2652081e9ba843940cfcc9c8fc77dee7c3ea1ab4
deissh <deissh@yandex.ru>
2020-09-19 13:12:17 +0300

    feat: add pyproject.toml (Poetry) and rewrite worker.py

--- worker.py
+++ worker.py
@@ -1,4 +1,23 @@
+from celery.bin import worker
+
 # noinspection PyUnresolvedReferences
 from src.entrypoint import entrypoint
 # noinspection PyUnresolvedReferences
 from src.tasks import *
+
+
+def run():
+    app = worker.worker(app=entrypoint)
+
+    options = {
+        'broker': 'amqp://guest:guest@localhost:5672//',
+        'loglevel': 'INFO',
+        'traceback': True,
+        'beat': True
+    }
+
+    app.run(**options)
+
+
+if __name__ == '__main__':
+    run()
