9848f6f0545e2456066c2fc5a3def76a4c1d2461
deissh <deissh@yandex.ru>
2020-12-21 21:07:43 +0300

    feat: calc pp and acc (oppai-ng -_-)

--- src/tasks/process_replay.py
+++ src/tasks/process_replay.py
@@ -10,7 +10,7 @@ from src.client import UpdateScoreRequestParsed
 from src.config import config
 from src.entrypoint import entrypoint
 
-from src.replay import ReplayParser
+from src.core.replay import ReplayParser, Performance
 from src.models.score import Score
 from src.models.replay import Replay
 from src.models.requsets.new_replay import NewReplayRequest
@@ -64,12 +64,13 @@ class ProcessReplay(Task):
             )
 
     def _process(self, req: NewReplayRequest) -> None:
-        replay_file = self._load_replay(config.storage.replays_path, req.key)
+        replay_file = self._load_replay(req.key)
+        beatmap_file = self._load_beatmap(req.beatmap.id)
 
         replay = self._parse_replay(replay_file)
         log.info(f"parsed replay: {replay.dict(exclude={'life_bar_graph', 'play_data'})}")
 
-        score = self._calculate(replay)
+        score = self._calculate(replay, beatmap_file)
         log.info(f"score: {score.dict(include={'pp', 'accuracy'})}")
 
         is_cheat = self._anticheat(replay, score)
@@ -81,27 +82,40 @@ class ProcessReplay(Task):
             log.info('save results')
             self._send_score(req, score)
 
-    def _load_replay(self, bucket: str, key: str) -> bytes:
+    @staticmethod
+    def _load_replay(key: str) -> bytes:
         log.info('loading replay from s3 bucket')
 
-        get_object_response = s3.get_object(Bucket=bucket, Key=key)
+        get_object_response = s3.get_object(
+            Bucket=config.storage.replays_path,
+            Key=key
+        )
         return get_object_response['Body'].read()
 
-    def _parse_replay(self, replay_data: bytes) -> Replay:
+    @staticmethod
+    def _load_beatmap(beatmap_id: int) -> bytes:
+        log.info('loading beatmap from s3 bucket')
+
+        get_object_response = s3.get_object(
+            Bucket=config.storage.beatmaps_path,
+            Key=str(beatmap_id) + ".osu"
+        )
+        return get_object_response['Body'].read()
+
+    @staticmethod
+    def _parse_replay(replay_data: bytes) -> Replay:
         return ReplayParser.from_bytes(replay_data)
 
-    def _calculate(self, replay: Replay) -> Score:
-        # TODO: this
-        return Score(
-            replay=replay,
-            accuracy=99.0,
-            pp=100,
-        )
+    @staticmethod
+    def _calculate(replay: Replay, beatmap_file: bytes) -> Score:
+        return Performance.process(replay, beatmap_file)
 
-    def _anticheat(self, replay: Replay, score: Score) -> bool:
+    @staticmethod
+    def _anticheat(replay: Replay, score: Score) -> bool:
         return False
 
-    def _send_score(self, req: NewReplayRequest, score: Score):
+    @staticmethod
+    def _send_score(req: NewReplayRequest, score: Score):
         body = src.client.UpdateScoreRequest(
             replay_id=req.replay_id,
             user_id=req.user.id,
@@ -127,6 +141,35 @@ if __name__ == '__main__':
     ProcessReplay.run({
         "replay_id": 1,
         "key": "replay-3af6b346-a185-46b0-af70-b5115f0f3507.osr",
+        "beatmap": {
+          "id": 100024,
+          "beatmapset_id": 1,
+          "mode": "osu",
+          "mode_int": 0,
+          "convert": False,
+          "difficulty_rating": 2.41,
+          "version": "Normal",
+          "total_length": 142,
+          "hit_length": 109,
+          "bpm": 119,
+          "cs": 4,
+          "drain": 6,
+          "accuracy": 6,
+          "ar": 6,
+          "playcount": 442616,
+          "passcount": 54419,
+          "count_circles": 160,
+          "count_sliders": 0,
+          "count_spinners": 0,
+          "count_total": 0,
+          "is_scoreable": True,
+          "last_updated": "2014-05-18T17:16:42Z",
+          "ranked": 1,
+          "status": "ranked",
+          "url": "https://osu.ppy.sh/beatmaps/75",
+          "deleted_at": "0001-01-01T00:00:00Z",
+          "max_combo": 314
+        },
         "user": {
             "avatar_url": "https://301222.selcdn.ru/akasi/avatars/1.png",
             "country_code": "RU",
